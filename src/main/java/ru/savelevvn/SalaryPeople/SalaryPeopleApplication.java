f246f711835fdc04e0193b70b46127b0baee7653
Vladimir <anykey196@gmail.com>
2024-10-13 16:13:25 +0500

    Init

---
+++ src/main/java/ru/savelevvn/SalaryPeople/SalaryPeopleApplication.java
@@ -0,0 +1,13 @@
+package ru.savelevvn.SalaryPeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryPeopleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/savelevvn/SalaryPeople/SalaryPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.savelevvn.SalaryPeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
